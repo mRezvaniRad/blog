ee87418b9de2845f6cbbbe312a51d9a680d8dcc3
marjan <m.rezvani.rad@gmail.com>
2020-11-26 11:02:58 +0330

    add

--- src/main/java/ir/arcademy/blog/BlogApplication.java
+++ src/main/java/ir/arcademy/blog/BlogApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApplication {
 
     public static void main(String[] args) {
+        System.out.println("salam");
+
         SpringApplication.run(BlogApplication.class, args);
+
     }
 
 }
--- src/main/java/ir/arcademy/blog/Test.java
+++ src/main/java/ir/arcademy/blog/Test.java
@@ -2,5 +2,8 @@ package ir.arcademy.blog;
 
 public class Test {
 
-    
+    public static void main(String[] args) {
+        System.out.println("helloo marjan joooooooooooon");
+    }
+
 }
