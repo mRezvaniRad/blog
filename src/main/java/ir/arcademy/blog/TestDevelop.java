87fdc10da46ea3b8f8f4a87a0bb00e4031a7b435
marjan <m.rezvani.rad@gmail.com>
2020-11-26 14:17:06 +0330

    add to develop

--- src/main/java/ir/arcademy/blog/TestDevelop.java
+++ src/main/java/ir/arcademy/blog/TestDevelop.java
@@ -1,6 +1,6 @@
 package ir.arcademy.blog;
 
-public class Test {
+public class TestDevelop {
 
     public static void main(String[] args) {
         System.out.println("helloo marjan joooooooooooon");
