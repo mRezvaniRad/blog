6699af4cb7643e068d835fcbacf8169d442358c1
marjan <m.rezvani.rad@gmail.com>
2020-11-27 09:16:10 +0330

    add for github

--- src/main/java/ir/arcademy/blog/TestDevelop.java
+++ src/main/java/ir/arcademy/blog/TestDevelop.java
@@ -4,6 +4,7 @@ public class TestDevelop {
 
     public static void main(String[] args) {
         System.out.println("helloo marjan joooooooooooon");
+        System.out.println("I get my wish");
     }
 
 }
